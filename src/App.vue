8b8d8d6d050320c6a462b2a4b1e09a625089c864
phpusr <phpusr@gmail.com>
2018-11-30 19:39:36 +0500

    change: Main page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="http://localhost:8081/img/logo.82b9c7a5.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Main />
   </div>
 </template>
 
 <script>
-    import HelloWorld from './components/HelloWorld.vue'
+    import Main from './pages/Main'
 
     export default {
-        name: 'app',
-        components: {
-            HelloWorld
-        }
+    name: 'app',
+    components: {
+        Main
     }
+}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/pages/Main.vue
@@ -0,0 +1,11 @@
+<template>
+    <h3>Main page</h3>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
