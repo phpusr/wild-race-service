dbf975818b335478acd9862aed6eabbf9bc20f53
phpusr <phpusr@gmail.com>
2018-11-30 12:16:59 +0500

    add: Класс для хранения настроек

---
+++ src/main/kotlin/com/phpusr/wildrace/domain/data/Config.kt
@@ -0,0 +1,34 @@
+package com.phpusr.wildrace.domain.data
+
+import org.hibernate.validator.constraints.Length
+
+/**
+ * Настройки приложения
+ */
+class Config(
+        /** Синхронизация постов */
+        val syncPosts: Boolean,
+
+        /** Кол-во секунд между запусками синхронизации постов */
+        val syncSeconds: Int,
+
+        /** ID группы в VK */
+        val groupId: Long,
+
+        /** Название группы в VK (в URL) */
+        @field:Length(max = 100, message = "group_short_link_too_long")
+        val groupShortLink: String,
+
+        /** Комментирование статуса обработки постов */
+        val commenting: Boolean,
+
+        /** Токен доступа для комментирования */
+        @field:Length(max = 100, message = "comment_access_token_too_long")
+        val commentAccessToken: String,
+
+        /** Оставлять-ли комментарий от имени группы */
+        val commentFromGroup: Boolean,
+
+        /** Публиковать-ли статистику в ВК */
+        val publishStat: Boolean
+)
