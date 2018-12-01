2d517b44ab8052c6413e30b2e20f5a21685cb1c1
phpusr <phpusr@gmail.com>
2018-12-02 00:42:33 +0500

    add: Вывод статистики

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,5 +1,11 @@
 <template>
     <v-flex xs12 md6 offset-md3>
+        <v-layout justify-center>
+            <v-card v-for="v in stat" class="mx-1 pa-3 text-xs-center">
+                <div class="display-1">{{v.value}}</div>
+                <div>{{v.title}}</div>
+            </v-card>
+        </v-layout>
         <v-layout column>
             <post v-for="post in posts" :post="post" :key="post.id" />
         </v-layout>
@@ -10,23 +16,37 @@
     import Post from '../components/Post'
 
     export default {
-        components: {Post},
-        data: () => ({
-            posts: []
-        }),
-        created() {
-            this.fetchData();
-        },
-        methods: {
-            fetchData() {
-                this.$http.get('/post').then(response => {
-                    const {list, config} = response.body;
-                    this.posts = list;
-                    this.config = config;
-                });
-            }
+    components: {Post},
+    data: () => ({
+        posts: [],
+        numberOfRuns: 0,
+        sumDistance: 0,
+        total: 0
+    }),
+    created() {
+        this.fetchData();
+    },
+    methods: {
+        fetchData() {
+            this.$http.get('/post').then(response => {
+                const {list, numberOfRuns, sumDistance, total} = response.body;
+                this.posts = list;
+                this.numberOfRuns = numberOfRuns;
+                this.sumDistance = sumDistance;
+                this.total = total;
+            });
+        }
+    },
+    computed: {
+        stat() {
+            return [
+                {title: 'Всего пробежали (км)', value: this.sumDistance},
+                {title: 'Количество пробежек', value: this.numberOfRuns},
+                {title: 'Количество постов', value: this.total}
+            ]
         }
     }
+}
 </script>
 
 <style>
