b6e72f89fff0cbf2ebd549d832aefc38c02c28dd
phpusr <phpusr@gmail.com>
2018-12-07 18:03:01 +0500

    change: Test Page

--- src/pages/Test.vue
+++ src/pages/Test.vue
@@ -1,21 +1,21 @@
 <template>
-    <v-container grid-list-lg>
-        <v-layout column>
-            <v-flex xs6 v-for="i in 6">
-                <v-card color="cyan" class="white--text">
-                    <v-card-title primary-title>
-                        <h3>Test</h3>
-                    </v-card-title>
-                </v-card>
-            </v-flex>
-        </v-layout>
-    </v-container>
+    <v-flex xs6>
+        <v-container grid-list-sm fluid>
+            <v-layout wrap>
+                <v-flex d-flex v-for="i in list" :key="i" xs4>
+                    <v-card>
+                        <v-card-text>{{i}}</v-card-text>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </v-flex>
 </template>
 
 <script>
 export default {
     data: () => ({
-
+        list: ['Which text', 'text', 'random text with too long text how to turn around your opinion', 'and more text', 'age of emperies', 'a thousand miles', 'I here without baby', 'desapear now and your face']
     })
 }
 </script>
