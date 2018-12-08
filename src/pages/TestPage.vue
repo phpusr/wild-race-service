3ec0bc32c57e173e6be24d639c11e7f3c10d992c
phpusr <phpusr@gmail.com>
2018-12-08 12:41:57 +0500

    add: Date picker examples

--- src/pages/TestPage.vue
+++ src/pages/TestPage.vue
@@ -2,10 +2,50 @@
     <v-flex xs6>
         <v-container grid-list-sm fluid>
             <v-layout wrap>
-                <v-flex d-flex v-for="i in list" :key="i" xs4>
-                    <v-card>
-                        <v-card-text>{{i}}</v-card-text>
-                    </v-card>
+                <!-- Дату можно только выбирать с дата пикера -->
+                <v-flex xs12 sm6 md4>
+                    <v-menu
+                            :nudge-right="40"
+                            lazy
+                            transition="scale-transition"
+                            offset-y
+                            full-width
+                            min-width="290px"
+                    >
+                        <v-text-field
+                                slot="activator"
+                                :value="formatDate(date)"
+                                label="Picker in menu"
+                                prepend-icon="event"
+                                readonly
+                        />
+                        <v-date-picker v-model="date" no-title scrollable />
+                    </v-menu>
+                </v-flex>
+
+                <!-- Дату можно выбирать как с дата пикера, так и вводить вручную -->
+                <v-flex xs12 lg6>
+                    <v-menu
+                            :close-on-content-click="false"
+                            v-model="menu1"
+                            :nudge-right="40"
+                            lazy
+                            transition="scale-transition"
+                            offset-y
+                            full-width
+                            max-width="290px"
+                            min-width="290px"
+                    >
+                        <v-text-field
+                                slot="activator"
+                                v-model="dateFormatted"
+                                label="Date"
+                                prepend-icon="event"
+                                @blur="date = parseDate(dateFormatted)"
+                        />
+                        <v-date-picker v-model="date" no-title @input="menu1 = false" />
+                    </v-menu>
+                    <p>Date in ISO format: <strong>{{ date }} ({{ dateFormatted }})</strong></p>
                 </v-flex>
             </v-layout>
         </v-container>
@@ -13,11 +53,38 @@
 </template>
 
 <script>
-export default {
-    data: () => ({
-        list: ['Which text', 'text', 'random text with too long text how to turn around your opinion', 'and more text', 'age of emperies', 'a thousand miles', 'I here without baby', 'desapear now and your face']
-    })
-}
+    import dateFormat from 'date-format'
+
+    const ruDatePattern = 'dd.MM.yyyy';
+    const isoDatePattern = 'yyyy-MM-dd';
+    export default {
+
+        data: () => ({
+            menu: false,
+            menu1: false,
+            date: null,
+            dateFormatted: null
+        }),
+
+        watch: {
+            date (value) {
+                this.dateFormatted = this.formatDate(value)
+            }
+        },
+
+        methods: {
+            formatDate (date) {
+                if (!date) return null;
+
+                return dateFormat(ruDatePattern, new Date(date));
+            },
+            parseDate (date) {
+                if (!date) return null;
+
+                return dateFormat(isoDatePattern, dateFormat.parse(ruDatePattern, date))
+            }
+        }
+    }
 </script>
 
 <style>
