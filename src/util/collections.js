378a3e0aa8b70e7c17daf8d80b3bce2dc3899fa6
phpusr <phpusr@gmail.com>
2019-01-11 19:43:00 +0500

    remove: getIndex function

--- src/util/collections.js
+++ src/util/collections.js
@@ -1,23 +1,13 @@
 
-export function getIndex(list, id) {
-    for (let i = 0; i < list.length; i++) {
-        if (list[i].id === id) {
-            return i;
-        }
-    }
-
-    return -1;
-}
-
 export function replaceObject(list, object) {
-    const index = getIndex(list, object.id);
+    const index = list.findIndex(el => el.id === object.id);
     if (index > -1) {
         list.splice(index, 1, object);
     }
 }
 
 export function deleteObject(list, id) {
-    const index = getIndex(list, id);
+    const index = list.findIndex(el => el.id === id);
     if (index > -1) {
         list.splice(index, 1);
         return true;
