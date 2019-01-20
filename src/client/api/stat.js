861bb25a61b9b9a4172f3ccc0a36e7f1c24a4b3b
phpusr <phpusr@gmail.com>
2019-01-21 01:19:47 +0500

    fix: Transfer params in publish post request

--- src/client/api/stat.js
+++ src/client/api/stat.js
@@ -2,5 +2,5 @@ import Vue from "vue"
 
 export default {
     get: (params) => Vue.http.get("/stat", {params}),
-    publishPost: (params) => Vue.http.post("/stat/publishPost", params)
+    publishPost: (params) => Vue.http.post("/stat/publishPost", {}, {params})
 }
