40f9c98cf3c1c391c2e017a0c97a02218061575d
phpusr <phpusr@gmail.com>
2019-01-22 17:03:20 +0500

    change: Auto adding static files from resources

--- src/main/kotlin/com/phpusr/wildrace/controller/MainController.kt
+++ src/main/kotlin/com/phpusr/wildrace/controller/MainController.kt
@@ -3,6 +3,7 @@ package com.phpusr.wildrace.controller
 import com.phpusr.wildrace.domain.TempDataRepo
 import com.phpusr.wildrace.service.EnvironmentService
 import com.phpusr.wildrace.service.StatService
+import org.springframework.core.io.ClassPathResource
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Controller
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
+
 
 @Transactional(readOnly = true)
 @RequestMapping("/")
@@ -25,18 +27,29 @@ class MainController(
 
     @GetMapping
     fun main(model: Model, @AuthenticationPrincipal user: UserDetails?): String {
+        model["isProdMode"] = environmentService.isProduction
+        model["jsFiles"] = getFiles("js", "js")
+        model["cssFiles"] = getFiles("css", "css")
+
         model["frontendData"] = mapOf(
                 "user" to user,
                 "stat" to statService.getStat(),
                 "lastSyncDate" to tempDataRepo.get().lastSyncDate.time
         )
-        model["isProdMode"] = environmentService.isProduction
         return "index"
     }
 
     @GetMapping("authorize")
     fun authorize(request: HttpServletRequest, response: HttpServletResponse): String {
         return "authorize"
+    }
+
+    private fun getFiles(dir: String, ext: String): List<String> {
+        return ClassPathResource("static/$dir")
+                .file
+                .listFiles()
+                .filter{ it.name.endsWith(ext) }
+                .map{ it.name }
     }
 
 
