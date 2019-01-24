ed21a4d7ea2f64a10f6b8937f5c0eb29e0b5eccb
phpusr <phpusr@gmail.com>
2019-01-25 01:17:54 +0500

    improve: Menu

--- src/client/components/NavigationDrawer.vue
+++ src/client/components/NavigationDrawer.vue
@@ -1,82 +1,70 @@
 <template>
     <v-navigation-drawer :value="value" @input="$emit('input', $event)" clipped app dark>
-        <v-flex>
-            <v-container class="">
-                <v-toolbar flat class="transparent">
-                    <v-list class="pa-0">
-                        <v-list-tile avatar>
-                            <v-list-tile-avatar>
-                                <img :src="defaultAvatar"  alt="Default avatar" />
-                            </v-list-tile-avatar>
+        <v-toolbar flat class="transparent">
+            <v-list class="pt-4" :class="mobile ? 'pl-0' : 'pl-4'">
+                <v-list-tile avatar>
+                    <v-list-tile-avatar>
+                        <img :src="defaultAvatar"  alt="Default avatar" />
+                    </v-list-tile-avatar>
 
-                            <v-list-tile-content>
-                                <v-list-tile-title v-if="user">{{user.username}}</v-list-tile-title>
-                                <login-dialog v-else />
-                            </v-list-tile-content>
+                    <v-list-tile-content>
+                        <v-list-tile-title v-if="user">{{user.username}}</v-list-tile-title>
+                        <login-dialog v-else />
+                    </v-list-tile-content>
 
-                            <v-btn v-if="user" flat icon>
-                                <v-icon @click="logoutAction">exit_to_app</v-icon>
-                            </v-btn>
-                        </v-list-tile>
+                    <v-btn v-if="user" flat icon>
+                        <v-icon @click="logoutAction">exit_to_app</v-icon>
+                    </v-btn>
+                </v-list-tile>
+            </v-list>
+        </v-toolbar>
 
-                        <v-list-tile v-if="mobile && userIsAdmin" @click="syncPosts">
-                            <v-list-tile-action>
-                                <v-icon>sync</v-icon>
-                            </v-list-tile-action>
-                            <v-list-tile-content>
-                                <v-list-tile-title>{{$t("sync.title")}}</v-list-tile-title>
-                            </v-list-tile-content>
-                        </v-list-tile>
+        <router-view name="menu" class="px-4 mt-5" />
 
-                        <v-list-tile v-if="mobile && userIsAdmin" to="/config">
-                            <v-list-tile-action>
-                                <v-icon>settings</v-icon>
-                            </v-list-tile-action>
-                            <v-list-tile-content>
-                                <v-list-tile-title>{{$t("pages./config")}}</v-list-tile-title>
-                            </v-list-tile-content>
-                        </v-list-tile>
+        <v-list class="mt-3">
+            <v-list-tile v-if="mobile && userIsAdmin" @click="syncPosts">
+                <v-list-tile-action>
+                    <v-icon>sync</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                    <v-list-tile-title>{{$t("sync.title")}}</v-list-tile-title>
+                </v-list-tile-content>
+            </v-list-tile>
 
-                        <v-list-tile v-if="mobile" to="/stat">
-                            <v-list-tile-action>
-                                <v-icon>signal_cellular_alt</v-icon>
-                            </v-list-tile-action>
-                            <v-list-tile-content>
-                                <v-list-tile-title>{{$t("pages./stat")}}</v-list-tile-title>
-                            </v-list-tile-content>
-                        </v-list-tile>
+            <v-list-tile v-if="mobile && userIsAdmin" to="/config">
+                <v-list-tile-action>
+                    <v-icon>settings</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                    <v-list-tile-title>{{$t("pages./config")}}</v-list-tile-title>
+                </v-list-tile-content>
+            </v-list-tile>
 
-                        <v-list-tile v-if="mobile" :href="config.groupLink" target="_blank" exact>
-                            <v-list-tile-action class="font-weight-bold">VK</v-list-tile-action>
-                            <v-list-tile-content>
-                                <v-list-tile-title>{{$t("pages.groupTitle")}}</v-list-tile-title>
-                            </v-list-tile-content>
-                        </v-list-tile>
+            <v-list-tile v-if="mobile" to="/stat">
+                <v-list-tile-action>
+                    <v-icon>signal_cellular_alt</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                    <v-list-tile-title>{{$t("pages./stat")}}</v-list-tile-title>
+                </v-list-tile-content>
+            </v-list-tile>
 
-                        <v-list-tile class="mt-2">
-                            <v-list-tile-action>
-                                <span class="subheading grey--text text--lighten-1">
-                                    <span class="font-weight-medium">{{$t("post.lastSyncDate")}}: </span>
-                                    <span>{{lastSyncDate}}</span>
-                                </span>
-                            </v-list-tile-action>
-                        </v-list-tile>
+            <v-list-tile v-if="mobile" :href="config.groupLink" target="_blank" exact>
+                <v-list-tile-action class="font-weight-bold">VK</v-list-tile-action>
+                <v-list-tile-content>
+                    <v-list-tile-title>{{$t("pages.groupTitle")}}</v-list-tile-title>
+                </v-list-tile-content>
+            </v-list-tile>
 
-                        <v-list-tile>
-                            <v-list-tile-action>
-                                <router-view name="menu" />
-                            </v-list-tile-action>
-                        </v-list-tile>
-                    </v-list>
-                </v-toolbar>
-            </v-container>
-        </v-flex>
-
-        <v-flex xs11>
-            <v-container class="pr-0">
-
-            </v-container>
-        </v-flex>
+            <v-list-tile class="mt-2">
+                <v-list-tile-action>
+                    <span class="subheading grey--text text--lighten-1">
+                        <span class="font-weight-medium">{{$t("post.lastSyncDate")}}: </span>
+                        <span>{{lastSyncDate}}</span>
+                    </span>
+                </v-list-tile-action>
+            </v-list-tile>
+        </v-list>
     </v-navigation-drawer>
 </template>
 
