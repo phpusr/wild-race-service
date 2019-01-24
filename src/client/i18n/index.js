844774342962dea13589889b376004c134fbac54
phpusr <phpusr@gmail.com>
2019-01-25 00:28:41 +0500

    change: Move init i18n to a separate file

--- src/client/i18n/index.js
+++ src/client/i18n/index.js
@@ -1,6 +1,17 @@
+import Vue from "vue"
+import VueI18n from "vue-i18n"
 import ru from "./ru"
 
-export default {
+const messages = {
     ru
 }
+
+Vue.use(VueI18n)
+
+const i18n = new VueI18n({
+    locale: "ru",
+    messages
+})
+
+export default i18n
 
--- src/client/main.js
+++ src/client/main.js
@@ -3,9 +3,8 @@ import "./api/resource"
 import App from "./App.vue"
 import Vuetify from "vuetify"
 import VueRouter from "vue-router"
-import VueI18n from "vue-i18n"
 import router from "./router"
-import messages from "./i18n"
+import i18n from "./i18n"
 import {connectToWS} from "./util/ws"
 import store from "./store"
 
@@ -17,12 +16,6 @@ Vue.config.productionTip = false
 
 Vue.use(Vuetify)
 Vue.use(VueRouter)
-Vue.use(VueI18n)
-
-const i18n = new VueI18n({
-    locale: "ru",
-    messages
-})
 
 new Vue({
     render: h => h(App),
