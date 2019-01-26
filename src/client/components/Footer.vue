7520619f3b113eef24620a3ab4b8818e1b462f23
phpusr <phpusr@gmail.com>
2019-01-26 19:19:16 +0500

    improve: Footer view

--- src/client/components/Footer.vue
+++ src/client/components/Footer.vue
@@ -1,11 +1,15 @@
 <template>
     <v-footer dark class="pa-4">
         <v-layout justify-center>
-            <div class="mt-1 font-weight-bold">{{$t("default.projectName")}} v{{config.projectVersion}}</div>
-            <div class="mt-1 ml-2">
-                Created by <a href="http://vk.com/phpusr">Sergey Doronin</a>
+            <div class="text-block">
+                <v-layout column class="text-block text-xs-center">
+                    <div class="font-weight-bold">{{$t("default.projectName")}} v{{config.projectVersion}}</div>
+                    <div>
+                        Created by <a href="http://vk.com/phpusr">Sergey Doronin</a>
+                    </div>
+                </v-layout>
             </div>
-            <div class="ml-2">
+            <div class="pt-1">
                 <a href="https://github.com/phpusr/wild-race-service" target="_blank">
                     <img src="/img/GitHub-Mark-32px.png" class="logo" alt="GitHub logo"/>
                 </a>
@@ -23,8 +27,11 @@
 </script>
 
 <style>
+    .text-block {
+        width: 185px;
+    }
     .logo {
         filter: invert(100%);
-        height: 25px;
+        height: 32px;
     }
 </style>
