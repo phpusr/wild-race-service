f1805b95bd9795f2068b9d72f93c97c65349374c
phpusr <phpusr@gmail.com>
2019-01-29 13:20:05 +0500

    change: Update WebSecurityConfigAccessTest

--- src/test/kotlin/com/phpusr/wildrace/config/WebSecurityConfigTest.kt
+++ src/test/kotlin/com/phpusr/wildrace/config/WebSecurityConfigTest.kt
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.ResultMatcher
@@ -17,6 +18,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @ActiveProfiles(profiles = [""])
+@TestPropertySource("/application-test.properties")
 @RunWith(SpringRunner::class)
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -44,7 +46,8 @@ class WebSecurityConfigAccessTest {
         checkAccess(put("/post/1"), status().isUnauthorized)
         checkAccess(delete("/post/1"), status().isUnauthorized)
 
-        checkAccess(get("/stat"), status().isOk)
+        checkAccess(get("/stat"), status().isBadRequest)
+        checkAccess(get("/stat?type=date"), status().isOk)
         checkAccess(put("/stat"), status().isUnauthorized)
 
         mockMvc.perform(get("/test")).andDo(print()).andExpect {
